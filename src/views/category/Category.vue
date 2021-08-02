b2f33ee598ed32eddfc11de2ce1c913af8cc1db8
long_mao_bu_re <1336283238@qq.com>
2021-08-02 19:30:28 +0800

    分类页面获取数据并报错

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,18 +1,120 @@
 <template>
-  <div>
-    <h1>分类模块</h1>
+  <div class="category">
+    <nav-bar class="category-nav">
+      <div slot="center">商品分类</div>
+    </nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories" @selectItem="selectItem"></tab-menu>
+    </div>
   </div>
 
 </template>
 
 <script>
 
+import NavBar from "../../components/common/navbar/NavBar";
+import TabMenu from "./childComps/TabMenu";
+import {getCategory, getSubcategory, getCategoryDetail} from "../../network/category";
+import {POP, NEW, SELL} from "../../common/const";
+
+
 export default {
   name: "Category",
-  components: {}
+  components: {
+    NavBar,
+    TabMenu
+  },
+  data() {
+    return {
+      categories: [],
+      currentIndex: -1,
+      categoryData: {},
+    }
+  },
+  created() {
+    this._getCategory();
+  },
+  methods: {
+    _getCategory() {
+      // 1. 请求类别资源
+      getCategory().then(res => {
+        this.categories = res.data.category.list;
+        // console.log(this.categories);
+        // 2.初始化每个类别的子数据
+        for (let i = 0; i < this.categories.length; i++) {
+          this.categoryData[i] = {
+            subcategories: {},
+            categoryDetail: {
+              'pop': [],
+              'new': [],
+              'sell': []
+            }
+          }
+        }
+        // console.log(this.categoryData)
+        this._getSubcategories(0);
+
+      })
+    },
+    // 根据类别获取对应资源并报错到对应数据中
+    _getSubcategories(index) {
+      this.currentIndex = index;
+      const mailKay = this.categories[index].maitKey;
+      getSubcategory(mailKay).then(res => {
+        // console.log(res);
+        // 保存请求回来的数据
+        this.categoryData[index].subcategories = res.data;
+        // console.log(this.categoryData);
+        this.categoryData = {...this.categoryData};
+        // console.log(this.categoryData);
+        // console.log(this.categoryData[index].subcategories)
+        this._getCategoryDetail(POP);
+        this._getCategoryDetail(NEW);
+        this._getCategoryDetail(SELL);
+      });
+    },
+    _getCategoryDetail(type) {
+      // 1.获取请求的miniWallkey
+      const miniWallkey = this.categories[this.currentIndex].miniWallkey;
+      // console.log(this.categories[this.currentIndex])
+      // console.log(type,miniWallkey);
+      // 2.发送请求,传入miniWallkey和type
+      getCategoryDetail(miniWallkey, type).then(res => {
+        // console.log(res);
+        // 3.将获取的数据保存下来
+        this.categoryData[this.currentIndex].categoryDetail[type] = res;
+        this.categoryData = {...this.categoryData};
+      });
+    },
+    /**
+     * 事件响应相关的方法
+     */
+    selectItem(index) {
+      // console.log(index);
+      this._getSubcategories(index);
+    }
+  }
 }
 </script>
 
 <style scoped>
+.category {
+  height: 100vh;
+}
 
+.category-nav {
+  background-color: var(--color-tint);
+  color: white;
+  padding-top: 5px;
+}
+
+.content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 44px;
+  bottom: 49px;
+  overflow: hidden;
+  display: flex;
+}
 </style>
---
+++ src/views/category/childComps/TabMenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <scroll class="tab-menu">
+    <div class="menu-list">
+      <div class="menu-list-item"
+           v-for="(item,index) in categories"
+           :key="index"
+           :class="{active: index===currentIndex}" @click="itemClick(index)">
+        {{ item.title }}
+      </div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import Scroll from "../../../components/common/scroll/Scroll";
+
+export default {
+  name: "TabMenu",
+  props: {
+    categories: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  components: {
+    Scroll
+  },
+  methods: {
+    itemClick(index) {
+      // 点击后传入当前index, 赋值给 currentIndex 切换active类名
+      this.currentIndex = index;
+      // $emit向父组件发送数据
+      this.$emit('selectItem', index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-menu {
+  background-color: #f6f6f6;
+  height: 100%;
+  width: 100px;
+  box-sizing: border-box;
+}
+
+.menu-list-item {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 14px;
+}
+
+.menu-list-item.active {
+  font-weight: 700;
+  color: var(--color-high-text);
+  background-color: #fff;
+  border-left: 3px solid var(--color-high-text);
+}
+</style>
